6b3501c2987668005cff49f4e6d6ff0a4a5c9cf1
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-12-01 11:06:59 +0100

    Notification app in JS

--- app/notification/js/app.js
+++ app/notification/js/app.js
@@ -1,7 +1,34 @@
-const greeting = new Notification('Hi, how are you?', {
-    body: 'Hav a nice day',
-});
-
-greeting.addEventListener('click', function() {
-    window.open("https://translate.google.com/")
-})
+(async () => {
+    const showNotification = () => {
+        const notification = new Notification('Javascript Notification Test', {
+            body: 'This is a Javascript notification',
+            icon: './img/js.png',
+            vibrate: true
+        });
+    
+        setTimeout(() => {
+            notification.close();
+        }, 10 * 1000);
+    
+        notification.addEventListener('click', () => {
+            window.open("https://translate.google.com/")
+        });
+    }
+    
+    const showError = () => {
+        const error = document.querySelector('.error');
+        error.style.display = 'block';
+        error.textContent = 'You blocked the notifications';
+    }
+    
+    let granted = false;
+    
+    if (Notification.permission === 'granted') {
+        granted = true;
+    } else if (Notification.permission !== 'denied') {
+        let permission = await Notification.requestPermission();
+        granted = permission === 'granted' ? true : false;
+    }
+    
+    granted ? showNotification() : showError();
+})();
